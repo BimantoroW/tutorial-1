a569a463b93aaf4daeb616d3df0d76c08bcce51c
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-02-13 10:11:50 +0700

    add tests for create and edit pages

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -8,13 +8,16 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.*;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerTest {
@@ -33,4 +36,45 @@ public class ProductControllerTest {
                 .andExpect(model().attributeExists("product"))
                 .andExpect(view().name("createProduct"));
     }
+
+    @Test
+    void testCreateProductPost() throws Exception {
+        this.mockMvc.perform(post("/product/create")
+                .accept(MediaType.APPLICATION_JSON)
+                        .param("productName", "yeehaw")
+                        .param("productQuantity", "123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", equalTo("list")));
+        verify(service, times(1)).create(any(Product.class));
+    }
+
+    @Test
+    void testEditProductGet() throws Exception {
+        Product product = new Product();
+        product.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product.setProductName("Sampo Cap Bambang");
+        product.setProductQuantity(100);
+
+        when(service.findById(anyString())).thenReturn(product);
+
+        this.mockMvc.perform(get(String.format("/product/edit?id=%s", product.getProductId())))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("product"))
+                .andExpect(content().string(containsString(product.getProductId())))
+                .andExpect(content().string(containsString(Integer.toString(product.getProductQuantity()))));
+    }
+
+    @Test
+    void testEditProductPost() throws Exception {
+        this.mockMvc.perform(post("/product/edit")
+                        .param("productId", "abcde")
+                        .param("productName", "yeehaw")
+                        .param("productQuantity", "123"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", equalTo("list")));
+        verify(service, times(1)).edit(any(Product.class), anyString(), anyInt());
+    }
+
 }
