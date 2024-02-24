8567c6351d6927444dea87ff93e314d62a618988
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-02-24 17:36:23 +0700

    split CarController into its own file

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/CarController.java
@@ -0,0 +1,59 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import id.ac.ui.cs.advprog.eshop.service.CarServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/car")
+public class CarController extends ProductController {
+    private static final String REDIRECT_LIST = "redirect:listCar";
+
+    @Autowired
+    private CarServiceImpl carService;
+
+    @GetMapping("/createCar")
+    public String createCarPage(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "CreateCar";
+    }
+
+    @PostMapping("/createCar")
+    public String createCarPost(@ModelAttribute Car car, Model model) {
+        carService.create(car);
+        return REDIRECT_LIST;
+    }
+
+    @GetMapping("/listCar")
+    public String carListPage(Model model) {
+        List<Car> allCars = carService.findAll();
+        model.addAttribute("cars", allCars);
+        return "CarList";
+    }
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carService.findById(carId);
+        model.addAttribute("car", car);
+        return "EditCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCarPost(@ModelAttribute Car car, Model model) {
+        carService.update(car.getCarId(), car);
+        return REDIRECT_LIST;
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCarDelete(@RequestParam("carId") String carId) {
+        carService.deleteCarById(carId);
+        return REDIRECT_LIST;
+    }
+}
