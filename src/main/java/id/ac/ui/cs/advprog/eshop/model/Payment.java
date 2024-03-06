798648b26df0facc17f1f85c398c5ef415883143
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-03-06 22:30:36 +0700

    [GREEN] Implement Payment model class

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
@@ -12,6 +14,37 @@ public class Payment {
     private Map<String, String> paymentData;
 
     public Payment(String id, String method, Map<String, String> paymentData) {
+        this.id = id;
+        setMethod(method);
+        if (paymentData.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        this.paymentData = paymentData;
+        if (this.isPaymentDataValid(paymentData)) {
+            this.setStatus(PaymentStatus.SUCCESS.getValue());
+        } else {
+            this.setStatus(PaymentStatus.REJECTED.getValue());
+        }
+    }
 
+    private void setMethod(String method) {
+        if (PaymentMethod.contains(method)) {
+            this.method = method;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void setStatus(String status) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    protected boolean isPaymentDataValid(Map<String, String> paymentData) {
+        // Override this in every subclass please please please please please please please don't forget okay bye
+        return true;
     }
 }
